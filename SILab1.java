a7cb3774113a7e1d97363c0c70bc9e210b883aab
Aleksandar Lazarevski <aleksandarl@marketingplatform.com>
2023-03-29 13:46:35 +0200

    Fix wrong loop for reading

--- SILab1.java
+++ SILab1.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.*;
 
 public class SILab1 {
     public static List<Integer> filterOddNumbers(List<Integer> list) {
@@ -29,9 +30,9 @@ public class SILab1 {
         int n = sc.nextInt();
 
         List<Integer> list = new ArrayList<>();
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i <= n; i++) {
-            list.add(sc.nextInt());
+            list.add(Integer.parseInt(br.readLine()););
         }
 
         System.out.println(filterOddNumbers(list));
